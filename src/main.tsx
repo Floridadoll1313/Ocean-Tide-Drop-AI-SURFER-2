69ea8b30ec036f28e33b97fcbe4a557c37998fc8
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-05-27 10:55:16 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,27 +4,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDi-h1ccOtyFq3h2uWq037pPiE76fX4wNk",
-  authDomain: "otdaisurfer-org.firebaseapp.com",
-  projectId: "otdaisurfer-org",
-  storageBucket: "otdaisurfer-org.firebasestorage.app",
-  messagingSenderId: "534903080702",
-  appId: "1:534903080702:web:b392690ad1468185d987af",
-  measurementId: "G-HGSEPKW1L4"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// We don't need to import Firebase specific initialization here anymore.
+// Our `src/firebase.ts` file handles that.
 
 // Now you can render your React app
 ReactDOM.createRoot(document.getElementById('root')!).render(
