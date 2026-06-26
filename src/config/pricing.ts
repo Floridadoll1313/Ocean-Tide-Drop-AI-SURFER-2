b1c74cceb289484851c3f89fbe9de86f7d3713fc
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-06-26 15:55:58 -0400

    Create src/config/pricing.ts

---
+++ src/config/pricing.ts
@@ -0,0 +1,22 @@
+export const PRICING = {
+  free: {
+    level: 0,
+    name: "Free",
+  },
+  bronze: {
+    level: 1,
+    name: "Bronze",
+  },
+  wave: {
+    level: 2,
+    name: "Wave",
+  },
+  tsunami: {
+    level: 3,
+    name: "Tsunami",
+  },
+  enterprise: {
+    level: 4,
+    name: "Enterprise",
+  },
+};
