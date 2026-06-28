64e499555d4a52783aacbcddd5f5813092b0237c
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-06-28 14:32:54 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,6 @@
-// src/firebase.js
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDna4Fq9NSezv-i7AiBWZLmN6ttdP7G25I",
@@ -14,5 +13,5 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore(app);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
