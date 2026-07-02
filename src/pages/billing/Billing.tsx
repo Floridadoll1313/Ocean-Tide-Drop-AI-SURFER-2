7e342cc69dd31e1df88d22cf0d99a1bc54daf8cb
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-07-02 17:44:21 -0400

    Update Billing.tsx

--- src/pages/billing/Billing.tsx
+++ src/pages/billing/Billing.tsx
@@ -13,7 +13,7 @@ export default function Billing() {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
-          customerId: userData?.uid, 
+          customerId: userData?.uid,
         }),
       });
 
