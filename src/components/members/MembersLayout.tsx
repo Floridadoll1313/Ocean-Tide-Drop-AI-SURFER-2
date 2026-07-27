6c5982b01fe699700ed1f1cae02887f72aefe229
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-07-27 17:15:24 -0400

    Update MembersLayout.tsx

--- src/components/members/MembersLayout.tsx
+++ src/components/members/MembersLayout.tsx
@@ -2,11 +2,13 @@ import { Outlet } from "react-router-dom";
 
 export default function MembersLayout() {
   return (
-    <div className="min-h-screen bg-slate-950 text-white">
+    <div className="min-h-screen bg-red-900 text-white">
 
-      <main>
-        <Outlet />
-      </main>
+      <h1 className="text-4xl p-10">
+        MEMBERS LAYOUT IS LOADING
+      </h1>
+
+      <Outlet />
 
     </div>
   );
